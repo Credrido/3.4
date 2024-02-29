65e57e45e32e0fb8217f6f67ba880721e4bd5442
Credrido <156992589+Credrido@users.noreply.github.com>
2024-02-29 10:25:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool randif;
+
+            IArrayBase[] arrays = new IArrayBase[2];
+
+            Console.WriteLine("Хотите заполнить массивы сами? Введите Да или Нет");
+
+            if (Console.ReadLine() == "Да")
+            {
+                randif = true;
+            }
+            else
+            {
+                randif = false;
+            }
+
+            arrays[0] = new Onedimensional(randif);
+            arrays[1] = new Twodimensional(randif);
+
+            foreach (ArrayBase array in arrays)
+            {
+                array.Print();
+                array.Getmiddle();
+            }
+
+
+            Console.WriteLine();
+
+            IPrinter[] arraysP = new IPrinter[3];
+
+            arraysP[0] = new DaysOfWeek();
+            arraysP[1] = new Onedimensional(randif);
+            arraysP[2] = new Twodimensional(randif);
+
+            foreach (IPrinter arr in arraysP)
+            {
+                arr.Print();
+            }
+
+        }
+    }
+}
---
+++ Twodimensional.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_3
+{
+    public sealed class Twodimensional : ArrayBase
+    {
+        private int[,] array;
+
+        public Twodimensional(bool randif) : base(randif) { }
+
+        protected override void Fillarray()
+        {
+            Console.WriteLine("Введите кол-во строк:");
+
+            int rows = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Введите кол-во столбцов:");
+
+            int columns = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            array = new int[rows, columns];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.WriteLine("Введите элемент массива:");
+
+                    array[i, j] = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
+        }
+
+        protected override void Fillarrayrandomly()
+        {
+            Random random = new Random();
+
+            array = new int[4, 4];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(-250, 250);
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public override void Getmiddle()
+        {
+            decimal sum = 0;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    sum += array[i, j];
+                }
+            }
+
+            decimal result = sum / (array.GetLength(0) * array.GetLength(1));
+
+            Console.WriteLine("Среднее значение массива:");
+
+            Console.WriteLine(result);
+
+            Console.WriteLine();
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Итоги двумерного массива");
+            Console.WriteLine();
+            Console.WriteLine("Исходный массив:");
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write($"{array[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
