76a4d8c34c193b41728a2f0eac55d46989e25b5f
Credrido <156992589+Credrido@users.noreply.github.com>
2024-03-06 19:22:15 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,43 +6,7 @@ namespace _3_4
     {
         static void Main(string[] args)
         {
-            bool randif;
-
-            IArrayBase[] arrays = new IArrayBase[2];
-
-            Console.WriteLine("Хотите заполнить массивы сами? Введите Да или Нет");
-
-            if (Console.ReadLine() == "Да")
-            {
-                randif = true;
-            }
-            else
-            {
-                randif = false;
-            }
-
-            arrays[0] = new Onedimensional(randif);
-            arrays[1] = new Twodimensional(randif);
-
-            foreach (ArrayBase array in arrays)
-            {
-                array.Print();
-                array.Getmiddle();
-            }
-
-
-            Console.WriteLine();
-
-            IPrinter[] arraysP = new IPrinter[2];
             
-            arraysP[0] = new Onedimensional(randif);
-            arraysP[1] = new Twodimensional(randif);
-
-            foreach (IPrinter arr in arraysP)
-            {
-                arr.Print();
-            }
-
         }
     }
 }
